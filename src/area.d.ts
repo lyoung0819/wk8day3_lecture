b5815a76b9c22e4c4ba0bae87722540326f55cfc
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-10 15:27:50 -0500

    config changes made

---
+++ src/area.d.ts
@@ -0,0 +1,7 @@
+/*
+    Name of file should be the same as the file that we are
+    declaring type for with the .d.ts extension 
+    Example 1: abc.js --> abc.d.ts
+*/
+
+export function squareFootage(length:number, width: number):number 
---
+++ src/area.js
@@ -0,0 +1,13 @@
+// Pure JavaScript - No TypeScript
+
+// tell TS not to check this file (below is not a comment, it is for TS)
+
+
+// @ts-nocheck
+function squareFootage(length, width){
+    return length*width
+}
+
+export {
+    squareFootage
+}
